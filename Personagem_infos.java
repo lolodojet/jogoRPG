d5edfcfd7fb2222f2c6c13346bcaac06a582ca7c
heloisa <depaulamariaheloisa@gmail.com>
2025-08-06 11:53:06 -0300

    ordem de comandos organizados

--- Personagem_infos.java
+++ Personagem_infos.java
@@ -7,6 +7,7 @@ public class Personagem_infos{
         Scanner ler = new Scanner(System.in);
 
         Guerreiro guerreiro = new Guerreiro();
+        guerreiro.desenharPersonagem();
         System.out.println("Informe o nome do seu guerreiro: ");
         guerreiro.setNome(ler.nextLine());
         guerreiro.setClasse("Guerreiro");
@@ -15,8 +16,10 @@ public class Personagem_infos{
         guerreiro.setForca(0);
         guerreiro.setTipoDeArma("Espada");
         guerreiro.setArmadura("Encantada");
-
+        System.out.println();
+        
         Arqueiro arqueiro = new Arqueiro();
+        arqueiro.desenharPersonagem();
         arqueiro.setNome("Garen");
         arqueiro.setClasse("Arqueiro");
         arqueiro.setNivel(1);
@@ -25,9 +28,10 @@ public class Personagem_infos{
         arqueiro.setQuantidadeFlechas(10);
         arqueiro.setPrecisao(3);
         arqueiro.setFlechaEspecial("Flecha flamejante");
-
-
-        System.out.println("== A Guerra Vai Começar ==");
+        System.out.println();
+        System.out.println("Seu inimigo é Garen, um arqueiro mágico!");
+        System.out.println("-----------------------------");
+        System.out.println("==> A Guerra Vai Começar <==");
         
         boolean jogoLigado = true;
         boolean ataqueInicial = true;
@@ -39,12 +43,12 @@ public class Personagem_infos{
                 arqueiro.setQuantidadeFlechas(7);
                 arqueiro.setPrecisao(2);
                 guerreiro.receberDano(8);
+                System.out.println("Garen atirou flechas flamejantes, contra ataque!");
                 ataqueInicial = false;
             }
-
-            System.out.println("=== MENU ===");
-            System.out.println();
-            System.out.println("1 - Garen atirou flechas flamejantes, contra ataque!");
+            System.out.println("-----------------------------");
+            System.out.println("=> MENU <=");
+            System.out.println("1 - Contra ataque!");
             System.out.println("2 - Revide o ataque especial de Garen, use sua ultimate!");
             System.out.println("3 - Ver status?");
             System.out.println("4 - Jogar de Novo?");
@@ -54,10 +58,9 @@ public class Personagem_infos{
 
             switch(escolha){
             
-            case 1: guerreiro.atacar(); arqueiro.receberDano(8); arqueiro.usarHabilidadeEspecial(); guerreiro.receberDano(18);  
-            break;
+            case 1: guerreiro.atacar(); arqueiro.receberDano(8); System.out.println(); System.out.println("Garen usou a ultimate para te atacar!");arqueiro.usarHabilidadeEspecial(); guerreiro.receberDano(18); break;
 
-            case 2: guerreiro.usarHabilidadeEspecial(); arqueiro.receberDano(15); guerreiro.subirNivel();
+            case 2: guerreiro.usarHabilidadeEspecial(); arqueiro.receberDano(15); System.out.println(); guerreiro.subirNivel();
             break;
 
             case 3: guerreiro.mostrarStatus();
@@ -68,7 +71,7 @@ public class Personagem_infos{
 
             if (escolhaContinuar == 1){
                 guerreiro.setVida(100); guerreiro.setForca(0); guerreiro.setNivel(1); arqueiro.setVida(100); arqueiro.setForca(0); arqueiro.setNivel(1); 
-                System.out.println("Jogo reiniciado. Os personagens foram resetados!");
+                System.out.println(); System.out.println("Jogo reiniciado. Os personagens foram resetados!"); System.out.println();
                 ataqueInicial = true;
                 
             } else if (escolhaContinuar == 2){
